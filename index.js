56c8b61c813fe07861140e52fb7d36a411958d18
Gabriel Sardina <gabinex15@gmail.com>
2022-06-10 22:43:48 -0300

    [finished] implementacion de end-point persona

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(cors({ origin: "https://localhost:4200" }));
 app.use("/api/agente", require("./routes/agente.route"));
 app.use("/api/libro", require("./routes/libro.route"));
 app.use("/api/transaccion", require("./routes/transaccion.route"));
+app.use("/api/persona", require("./routes/persona.route"));
 
 //* Setting
 app.set("port", process.env.PORT || 3000);
