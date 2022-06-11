cbe0a4dc135e6a67cbcd479cd3fd593040c3577c
Gabriel Sardina <gabinex15@gmail.com>
2022-06-11 17:21:21 -0300

    [add] route pasaje

---
+++ routes/pasaje.route.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const pasajeCtrl = require("../controllers/pasaje.controller");
+const router = Router();
+
+router.get("/", pasajeCtrl.getPasajes);
+router.post("/", pasajeCtrl.createPasaje);
+router.delete("/:id", pasajeCtrl.deletePasaje);
+router.put("/:id", pasajeCtrl.editPasaje);
+router.get("/:categoria", pasajeCtrl.getPorCategoria);
+
+//* 📦 Exportacion de Modulo 📦
+module.exports = router;
